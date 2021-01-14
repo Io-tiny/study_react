ddbcd464e753b1fe1eeadfa60fc348200eef2bfd
Io-tiny <414344787@qq.com>
2021-01-14 11:55:56 +0800

    重新提交

--- studyrecord/src/index.js
+++ studyrecord/src/index.js
@@ -1,38 +1,36 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
-import KeepAlive, { AliveScope } from "./keep";
-import UseContextTest from './useContextTest'
+// eslint-disable-next-line
+import React, { useContext, useLocalStore } from 'react';
+import ReactDOM from 'react-dom';
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      count: {count}
-      <button onClick={() => setCount((count) => count + 1)}>add</button>
-    </div>
-  );
+export const StoreContext = React.createContext(null);
+
+export const useStore = () => {
+  const store = React.useContext(StoreContext);
+  return store;
+}
+export const store = {
+  a: {
+    todo: '1'
+  },
+  b: {
+    todo: '3'
+  }
 }
 
-function App() {
-  const [show, setShow] = useState(true);
+
+const App = () => {
   return (
-    <>
-      <AliveScope>
-        <div>
-          <button onClick={() => setShow((show) => !show)}>Toggle</button>
-          <p>无 KeepAlive</p>
-          {show && <Counter />}
-          <p>有 KeepAlive</p>
-          {show && (
-            <KeepAlive id="Test">
-              <Counter />
-            </KeepAlive>
-          )}
-        </div>
-      </AliveScope>
-      {/* <UseContextTest /> */}
-    </>
+    <StoreContext.Provider value={store}>
+      <AA></AA>
+    </StoreContext.Provider>
   );
 }
+const AA = () => {
+  return (
+    <p>haha</p>
+  )
+}
 
-render(<App />, document.getElementById("root"))
+ReactDOM.render((
+  <App></App>
+), document.getElementById('root'))
